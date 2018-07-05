0323ecb022b09b07c9838ef8b77f700e8d4b22cd
Omar Gutiérrez <omargc.mus@gmail.com>
2018-07-04 23:34:24 -0400

    config

--- parking-frontend/config/prod.env.js
+++ parking-frontend/config/prod.env.js
@@ -1,9 +1,9 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://test.local.agetic.gob.bo/api/"',
-  GRAPHQL_URL: '"http://test.local.agetic.gob.bo/graphql/"',
-  AUTH_URL: '"http://test.local.agetic.gob.bo/app/auth"',
+  API_URL: '"http://138.68.61.35:3000/api/"',
+  GRAPHQL_URL: '"http://138.68.61.35:3000/graphql/"',
+  AUTH_URL: '"http://138.68.61.35:3000/auth"',
   TIME_SESSION_EXPIRED: 30,
   ONBEFOREUNLOAD: false,
   AUTH_TOKEN: '"Bearer"',
