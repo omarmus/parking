25e9177dd58f9c50f1b4a9506cf3c8ab140f0a64
Omar Gutierrez Condori <omargc.mus@gmail.com>
2019-02-05 09:17:19 -0400

    error date

--- parking-backend/src/domain/services/parking/Movimiento.js
+++ parking-backend/src/domain/services/parking/Movimiento.js
@@ -102,7 +102,7 @@ module.exports = function movimientoService (repositories, res) {
             data.hora_salida = moment().format('HH:mm');
 
             // Creando/Actualizando pago
-            let costo = await Pago.calcularTotal(moment(data.fecha_llegada, 'YYYY-MM-DD').format('YYYY-MM-DD'), item.hora_llegada, data.fecha_salida, data.hora_salida);
+            let costo = await Pago.calcularTotal(moment(data.fecha_llegada).format('YYYY-MM-DD'), item.hora_llegada, data.fecha_salida, data.hora_salida);
             console.log('COSTO TOTAL =========', costo);
             if (costo.code === -1) {
               return res.error(new Error(costo.data.message || costo.data));
